7886124df2d5aa2a8517ce163e3e70ea839fb7e7
Algans2 <89076578+Algans2@users.noreply.github.com>
2021-12-12 13:18:07 +0700

    Update aktivasi3.sh

--- aktivasi3.sh
+++ aktivasi3.sh
@@ -1,5 +1,5 @@
-curl -s -X POST "https://api.cloudflare.com/client/v4/zones/Ini diisi dengan API Zone ID/dns_records" \
-     -H "X-Auth-Email: Ini diisi e-mail kamu di cloudflare" \
-     -H "X-Auth-Key: ini di isi dengan Global API Key" \
+curl -s -X POST "https://api.cloudflare.com/client/v4/zones/f509237fbaef0073fa86b42d2653b0ea/dns_records" \
+     -H "X-Auth-Email: gojekggee@gmail.com" \
+     -H "X-Auth-Key: 7af5a163a4398bfec159d54a249b90f31d8a0" \
      -H "Content-Type: application/json" \
-     --data '{"type":"A","name":"unik.nasihosting.com","content":"ip server","ttl":120,"priority":10,"proxied":true}' > /dev/null
+     --data '{"type":"A","name":"unik.api-botwhatsapp.ga","content":"ip server","ttl":120,"priority":10,"proxied":true}' > /dev/null
